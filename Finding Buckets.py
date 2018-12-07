46a7ef9b0a89442923edb7813e02aa180519b939
NileshSuthar <nsnilesh071@gmail.com>
2018-12-07 23:55:14 +0530

    Delete Finding Buckets.py

--- Finding Buckets.py
+++
@@ -1,9 +0,0 @@
-def hashtable_get_bucket(htable,keyword):
-    flag = 0
-    for i in htable:
-        for j in i:
-            if j[0] == keyword:
-                flag = 1
-                return i
-    if(flag == 0):
-        return []
